9e6b66a769a60bd33b74e3f1c1e40acd17cefb66
claudi <claudi1618@gmail.com>
2018-03-25 19:13:12 -0600

    Files and Fear Unity File

---
+++ Scriptable Object Fear/Assets/Scripts/Character.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "New Character", menuName = "Character")]
+public class Character : ScriptableObject {
+
+	public string name;
+	public int health;
+
+	public string recoveryAbility;
+	public string element;
+
+	public void Print(){
+		Debug.Log("Her name is: " + name + " Health: " + health + " Her recovery ability is: "+ recoveryAbility
+		+ " Her current element is: " + element);
+	}
+}

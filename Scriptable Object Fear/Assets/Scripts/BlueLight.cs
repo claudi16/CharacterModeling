6be9793d4aa708ecc75423fa980893679ccc8cee
claudi <claudi1618@gmail.com>
2018-03-28 15:33:37 -0600

    Fear Scriptable Objects Complete

---
+++ Scriptable Object Fear/Assets/Scripts/BlueLight.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueLight : MonoBehaviour {
+
+	public Light blueLight;
+	void Start () {
+		blueLight = GetComponent<Light>();
+		blueLight.color -= Color.blue;
+	}
+	
+}
